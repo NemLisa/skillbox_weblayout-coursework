9651720d113fbcaabb9dbb58fec9f463f410953d
NemLisa <hursanda@gmail.com>
2020-12-28 17:21:52 +0500

    add gallery section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.location.hash = '';
+
 //   Swiper
 
 $('.intro__slider').slick({
@@ -11,62 +12,84 @@ $('.intro__slider').slick({
     autoplaySpeed:5000,
 });
 
-$('.gallery__album').slick({
+var $status = $('.gallery__pagingInfo');
+$('.gallery__album').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text(i + ' / ' + slick.slideCount);
+});
+
+$('.gallery__album--slides').slick({
     infinite:false,
     rows:2,
     slidesPerRow: 3,
-    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    responsive:[
-        {
-            breakpoint: 1645,
-            settings: {
-            rows:2,
-            slidesPerRow: 2,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        },
-    ]
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-btn gallery__slick-next slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn gallery__slick-prev slick-prev"></button>',
+
 })
 
-$('.editions__items').slick({
-    infinite:false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    responsive:[
-        {
-            breakpoint: 1400,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        },
-    ]
-})
+// Gallery
+const galleryAlbum = document.querySelector('.gallery__album');
+const galleryCard = document.querySelector('.gallery__item-card');
+
+galleryAlbum.onclick = function(event){
+    const target = event.target.closest('.gallery__album-item')
+    console.log(event.target.className);
+    const card = event.target.closest('.gallery__item-card');
+
+    if(!target) return;
+    if(!galleryAlbum.contains(target)) return;
+  
+    if(event.target.className == 'gallery__card-close btn-reset') {
+        card.classList.remove('is-active');
+        target.querySelector('.gallery__item-inner').classList.remove('is-active');
+        return;
+    }
+    target.querySelector('.gallery__item-card').classList.add('is-active');
+    target.querySelector('.gallery__item-inner').classList.add('is-active');
+}
+
+// $('.editions__items').slick({
+//     infinite:false,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+//     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+//     responsive:[
+//         {
+//             breakpoint: 1400,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2,
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//             }
+//         },
+//     ]
+// })
 
 
 
 // Burger
-document.querySelector('.header__burger-block').onclick = function(){
-    document.querySelector('.header__nav').classList.add('is-active');
-    document.querySelector('.header-top__container').classList.add('resize100');
-}
-document.querySelector('.header__nav-close').onclick = function(){
-    document.querySelector('.header__nav').classList.remove('is-active');
-    document.querySelector('.header-top__container').classList.remove('resize100');
-}
+// document.querySelector('.header__burger-block').onclick = function(){
+//     document.querySelector('.header__nav').classList.add('is-active');
+//     document.querySelector('.header-top__container').classList.add('resize100');
+// }
+// document.querySelector('.header__nav-close').onclick = function(){
+//     document.querySelector('.header__nav').classList.remove('is-active');
+//     document.querySelector('.header-top__container').classList.remove('resize100');
+// }
+
+
+// Choices
+const element = document.querySelector('.gallery__select');
+const choices = new Choices(element, {
+    searchEnabled: false,
+    itemSelectText: '',
+    shouldSort: false,
+});
